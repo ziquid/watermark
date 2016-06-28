8d394b32bcef038464790fe73e8bff32f0522414
Joseph Cheek <joseph@gdgwebworks.com>
2016-06-28 14:28:31 -0400

    better wildcard handling when files contain spaces. exit script if imagemagick exits with error (e.g., failure or ^C in terminal).

--- watermark.sh
+++ watermark.sh
@@ -1,7 +1,7 @@
 b=$(basename "$PWD")
-pics="$*"
-[ "$pics" = "" ] && pics="../*.*"
-for a in $pics; do
+[ $# -eq 0 ] && pics="../*.*" || pics=
+for a in "$@" $pics; do
   echo $a
-  convert "$a" -background 'rgb(66, 139, 202)' -resize 3840x2160 -gravity center -extent 3840x2160 -font Arial -pointsize 40 -draw "gravity southwest fill black text 12,12 '$b' fill white text 11,11 '$b' " "$b"_"$(basename "$a")"
+  convert "$a" -background 'rgb(66, 139, 202)' -resize 3840x2160 -gravity center -extent 3840x2160 -font Arial -pointsize 48 -draw "gravity southwest fill black text 12,7 '$b' fill white text 11,6 '$b' " "$b"_"$(basename "$a")"
+  [ $? -gt 0 ] && exit $?
 done
