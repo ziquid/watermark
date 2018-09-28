cf80f4f58d38648b2b543ba41b3fcc52245a710b
Joseph Cheek <joseph@ziquid.com>
2018-09-29 00:34:04 +0800

    support for an annotation in .watermark-annotation

--- watermark.sh
+++ watermark.sh
@@ -1,7 +1,13 @@
 b=$(basename "$PWD")
 [ $# -eq 0 ] && pics="../*.*" || pics=
+[ -e .watermark-annotation ] && annotate="-gravity northwest -fill black -annotate +12+7 @.watermark-annotation -fill white -annotate +11+6 @.watermark-annotation" || annotate=
 for a in "$@" $pics; do
   echo $a
-  convert "$a" -background 'rgb(66, 139, 202)' -resize 3840x2160 -gravity center -extent 3840x2160 -font Arial -pointsize 48 -draw "gravity southwest fill black text 12,7 '$b' fill white text 11,6 '$b' " "$b"_"$(basename "$a")"
+  convert "$a" -background 'rgb(66, 139, 202)' \
+  -resize 3840x2160 -gravity center -extent 3840x2160 \
+  -font Arial -pointsize 48 \
+  -draw "gravity southwest fill black text 12,7 '$b' fill white text 11,6 '$b' " \
+  $annotate \
+  "$b"_"$(basename "$a")"
   [ $? -gt 0 ] && exit $?
 done
